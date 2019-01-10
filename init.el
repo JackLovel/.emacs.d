18267c392a707941904ec18058ed2babe7909316
JackLovel <210172130@qq.com>
2019-01-10 15:34:44 +0800

    week2JsIde

--- init.el
+++ init.el
@@ -4,7 +4,67 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+
+;; when emacs version > 24, then add this melpa emacs source 
+;; source: 清华源
+(when (>= emacs-major-version 24)
+    (require 'package)
+    (package-initialize)
+    (add-to-list 'package-archives '("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/") t)
+    )
+
+(require 'cl)
+
+;;add what　ever packages you want here
+(defvar zilongshanren/packages '(
+				 company
+				 monokai-theme
+				 hungry-delete
+				 swiper
+				 counsel
+				 smartparens
+				 js2-mode
+				 nodejs-repl
+	)  "Default packages")
+
+;; package-selected-packages = zilongshanren/packages
+(setq package-selected-packages zilongshanren/packages)
+
+(defun zilongshanren/packages-installed-p ()
+    (loop for pkg in zilongshanren/packages
+          when (not (package-installed-p pkg)) do (return nil)
+          finally (return t)))
+
+(unless (zilongshanren/packages-installed-p)
+    (message "%s" "Refreshing package database...")
+    (package-refresh-contents)
+    (dolist (pkg zilongshanren/packages)
+      (when (not (package-installed-p pkg))
+        (package-install pkg))))
+
+;; hungry_delete: delete blank space 
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+;; config for smartparens
+(require 'smartparens-config)
+;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(smartparens-global-mode t)
+
+;; config for swiper
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+
+;; config for js2-mode
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
 
 ;;关闭工具栏
 (tool-bar-mode -1)
@@ -46,6 +106,15 @@
 ;; 选中单词会 然后输入字母会进行替换
 (delete-selection-mode t)
 
+;; match parentesis
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+;; display current line
+(global-hl-line-mode t)
+
+;; load theme
+(load-theme 'monokai t) ;; monokai
+
 ;; 更改显示字体大小 16pt
 ;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
 (set-face-attribute 'default nil :height 160)
@@ -62,7 +131,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company))))
+ '(custom-safe-themes
+   (quote
+    ("bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
